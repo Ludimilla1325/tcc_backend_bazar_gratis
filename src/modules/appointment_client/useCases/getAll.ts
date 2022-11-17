94a450789afe4ecd415a2c0361bed3408cd3a95e
Ricardo Filho <ricardinhogiasson16@gmail.com>
2022-11-17 14:24:37 -0300

    Listar COmpras do Usuario

--- src/modules/appointment_client/useCases/getAll.ts
+++ src/modules/appointment_client/useCases/getAll.ts
@@ -1,9 +1,19 @@
-import { Client_Appointment } from "@prisma/client";
+import { Client_Appointment, Prisma } from "@prisma/client";
 import { prisma } from "../../../Prisma/client";
 export async function getAll(clientId: number) {
-  const query = await prisma.client_Appointment.findMany({
-    where: { clientId },
-  });
+  const queryConstruct = ``;
+
+  const query = await prisma.$queryRaw(Prisma.sql`
+  SELECT Appointment.appointment_date, 
+Client_Appointment.id,
+(select sum(Purchase.quantity * value) from Purchase  inner join Product on Product.id = Purchase.productId where client_AppointmentId = Client_Appointment.id ) as value,
+ Client_Appointment.delivered 
+from Client_Appointment inner join Appointment on Appointment.id = Client_Appointment.appointmentId
+
+where Client_Appointment.clientId = ${clientId}
+;
+  
+  `);
 
   return {
     sucess: true,
